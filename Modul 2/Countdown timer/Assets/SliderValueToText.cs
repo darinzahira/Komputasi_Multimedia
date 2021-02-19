fe3f90ed243524b8d58d8008d56d991811b53b5e
darinzahira <zahiradarin@gmail.com>
2021-02-19 23:41:37 +0700

    pertemuan 2

---
+++ Modul 2/Countdown timer/Assets/SliderValueToText.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SliderValueToText : MonoBehaviour
+{
+    public Slider sliderUI;
+    private Text textSliderValue;
+
+    void Start()
+    {
+        textSliderValue = GetComponent<Text>();
+        ShowSliderValue();
+    }
+
+    public void ShowSliderValue(){
+        string sliderMessage = "Slider value = " + sliderUI.value;
+        textSliderValue.text = sliderMessage;
+    }
+}
---
+++ Modul 2/Panel/Assets/ArrangeActions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ArrangeActions : MonoBehaviour
+{
+    private RectTransform panelRectTransform;
+
+    void Start()
+    {
+        panelRectTransform = GetComponent<RectTransform>();
+    }
+
+    public void MoveDownOne()
+    {
+        print("(before change) " + gameObject.name + "sibling index = " + panelRectTransform.GetSiblingIndex());
+        int currentSiblingIndex = 
+        panelRectTransform.GetSiblingIndex();
+        panelRectTransform.SetSiblingIndex(currentSiblingIndex - 1);
+        print("(after change) " + gameObject.name + " sibling index = " + panelRectTransform.GetSiblingIndex());
+    }
+
+    public void MoveUpOne()
+    {
+        print("(before change) " + gameObject.name + "sibling index = " + panelRectTransform.GetSiblingIndex());
+        int currentSiblingIndex = 
+        panelRectTransform.GetSiblingIndex();
+        panelRectTransform.SetSiblingIndex(currentSiblingIndex + 1);
+        print("(after change) " + gameObject.name + " sibling index = " + panelRectTransform.GetSiblingIndex());
+    }
+}
